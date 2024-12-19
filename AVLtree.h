b09cdc96d78b3c735a1aa234dc461e11d1bba55c
AdiVolfman <advolfman@gmail.com>
2024-12-19 12:53:41 +0200

    added AVLtree class with code *SOLELY CREATED BY CHATGPT*

---
+++ AVLtree.h
@@ -0,0 +1,44 @@
+// AVLTree.h
+#ifndef AVLTREE_H
+#define AVLTREE_H
+
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+// Node structure
+struct Node {
+    int key;
+    Node *left;
+    Node *right;
+    int height;
+
+    Node(int val);
+};
+
+// AVL Tree class
+class AVLTree {
+private:
+    Node *root;
+
+    // Helper methods
+    int getHeight(Node *node);
+    int getBalance(Node *node);
+    Node *rotateRight(Node *y);
+    Node *rotateLeft(Node *x);
+    Node *insert(Node *node, int key);
+    Node *erase(Node *node, int key);
+    Node *minValueNode(Node *node);
+    bool search(Node *node, int key);
+    void inOrder(Node *node);
+
+public:
+    AVLTree();
+    void add(int key);
+    void erase(int key);
+    bool find(int key);
+    void printInOrder();
+};
+
+#endif
